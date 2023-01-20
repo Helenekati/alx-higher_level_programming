272453a8f58ed74d868fc45750bea655e039b6ac
Helenekati <ekatihelen@gmail.com>
2023-01-20 01:28:14 -0800

    add a new folder

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""my square module."""
+
+
+class Square:
+  """define a Square."""
+
+  def __str__(self):
+    """teach python to print the square my way"""
+    return self.pos_print()[:-1]
+
+  def __init__(self, size=0, position=(0, 0)):
+    """ initialize the square with this
+    Args:
+        size: a side of square
+        position: where the square is (coordinates)
+    """
+    self.size = size
+    self.position = position
+
+                                                                                                     @property
+    def size(self):
+     """property of the length of a side of square
+     Raises:
+        TypeError: if size is not an int.
+        ValueError: if size is < 0.
+    """
+    return self.__size
+
+                                                                                                     @size.setter
+    def size(self, value):
+    """ set the size of square
+    Args:
+        value: the size
+    Raises:
+        TypeError: if value is not int
+        ValueError: if valie < 0
+    """
+    if not isinstance(value, int):
+        raise TypeError('size must be an integer')                                              
+                                                                                                                            
