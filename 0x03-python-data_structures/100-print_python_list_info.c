39b582ab422d0f287edc6ab538ceffff2c3c7598
Helenekati <111124762+Helenekati@users.noreply.github.com>
2023-01-28 00:57:14 +0100

    Add files via upload

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -3,9 +3,9 @@
 #include <listobject.h>
 
 /**
- * print_python_list_info - prints some basic info about pytohn list
- * @p: python object
- **/
+* print_python_list_info - prints some basic info about pytohn list
+* @p: python object
+**/
 
 void print_python_list_info(PyObject *p)
 {
@@ -18,4 +18,3 @@ void print_python_list_info(PyObject *p)
 	for (i = 0; i < size; i++)
 		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
 }
-
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-
     if sentence != '':
         first_char = sentence[0]
-     else:
+    else:
         first_char = None
-     return (len(sentence), first_char)
-
+    return (len(sentence), first_char)
